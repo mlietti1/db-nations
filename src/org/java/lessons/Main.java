42a6d2f825beda9761e7a9c1fd8f4b803547b44b
mlietti1 <mcf.lietti@outlook.com>
2023-03-24 14:05:01 +0100

    bonus

--- src/org/java/lessons/Main.java
+++ src/org/java/lessons/Main.java
@@ -1,5 +1,6 @@
 package org.java.lessons;
 
+import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,8 +15,11 @@ public class Main {
     public static void main(String[] args) {
 
 
+        Scanner scan = new Scanner(System.in);
+
         try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)){
 
+            boolean hasResults = false;
             String query = """
                     SELECT c.name, c.country_id , r.name, c2.name  FROM countries c\s
                     JOIN regions r ON r.region_id = c.region_id\s
@@ -24,7 +28,21 @@ public class Main {
                     ORDER BY c.name;
                     """;
 
-            Scanner scan = new Scanner(System.in);
+            String queryLang = """
+                    SELECT l.`language` from country_languages cl\s
+                    join languages l ON l.language_id =cl.language_id\s
+                    where cl.country_id = ?
+                    ORDER BY l.`language`;
+                    """;
+
+            String queryStats = """
+                    SELECT `year` , population , gdp\s
+                    FROM country_stats cs\s
+                    WHERE cs.country_id = ?
+                    ORDER BY `year` DESC
+                    limit 1;
+                    """;
+
             System.out.print("Search a country: ");
             String searchString = scan.nextLine();
 
@@ -36,22 +54,30 @@ public class Main {
                 ps.setString(1, "%" + searchString + "%");
 
                 try (ResultSet rs = ps.executeQuery()) {
-                    while (rs.next()){
-                        String country = rs.getString("c.name");
-                        int countryId = rs.getInt("c.country_id");
-                        String region = rs.getString("r.name");
-                        String continent = rs.getString("c2.name");
+                    if(rs.next()){
 
-                        Nation nation = new Nation(country, countryId, region, continent);
+                        hasResults = true;
+                        System.out.printf("%45s", "Country");
+                        System.out.printf("%10s", "ID");
+                        System.out.printf("%30s", "Region");
+                        System.out.printf("%30s", "Continent");
+                        System.out.println("\n");
+                        do {
 
-                        nations.add(nation);
+                            String country = rs.getString("c.name");
+                            int countryId = rs.getInt("c.country_id");
+                            String region = rs.getString("r.name");
+                            String continent = rs.getString("c2.name");
+
+                            Nation nation = new Nation(country, countryId, region, continent);
+
+                            nations.add(nation);
+                        } while (rs.next());
+
+
+                    } else {
+                        System.out.println("No countries found.");
                     }
-
-                    System.out.printf("%45s", "Country");
-                    System.out.printf("%10s", "ID");
-                    System.out.printf("%30s", "Region");
-                    System.out.printf("%30s", "Continent");
-                    System.out.println("\n");
 
                     for (Nation n : nations){
                         printResult(n.getCountry(), n.getCountryId(), n.getRegion(), n.getContinent());
@@ -59,11 +85,46 @@ public class Main {
                 }
             }
 
+            if(hasResults){
+                System.out.print("Choose a country id: ");
+                int countryId = Integer.parseInt(scan.nextLine());
+
+                try(PreparedStatement psLang = connection.prepareStatement(queryLang)){
+                    psLang.setInt(1, countryId);
+                    try (ResultSet rsLang = psLang.executeQuery()){
+                        System.out.println("Languages: ");
+                        while (rsLang.next()){
+                            System.out.print(rsLang.getString(1));
+                            if(!rsLang.isLast()){
+                                System.out.print(", ");
+                            }else{
+                                System.out.println();
+                            }
+                        }
+                    }
+                }
+
+                try(PreparedStatement psStats = connection.prepareStatement(queryStats)){
+                    psStats.setInt(1, countryId);
+                    try(ResultSet rsStats = psStats.executeQuery()){
+                        if(rsStats.next()){
+                            int year = rsStats.getInt(1);
+                            BigDecimal population = rsStats.getBigDecimal(2);
+                            BigDecimal gdp = rsStats.getBigDecimal(3);
+                            System.out.println("Stats for year " + year + ":");
+                            System.out.println("Population: " + population);
+                            System.out.println("GDP: " + gdp);
+                        }
+                    }
+                }
+
+            }
+
+
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
 
     }
     private static void printResult(String country, int countryId, String region, String continent){
